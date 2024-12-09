ff5fe76fe98849c40167193027f65934fc59e67c
ExGranite <73008449+ExGranite@users.noreply.github.com>
2024-12-08 23:00:19 -0700

    reset

--- src/da/jade/project/Artwork.java
+++
@@ -1,9 +0,0 @@
-package da.jade.project;
-
-public class Artwork {
-	public int paymentId;
-	public String name, description, genre;
-	public double price;
-	public boolean status;
-	public int artistId, ownerId;
-}
